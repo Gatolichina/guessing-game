2697521130f15105e9b084e055d47bae8ba4ddb1
Gatolichina <antonchikg@gmail.com>
2017-02-01 22:11:17 +0300

    GuessingGame:made game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+    	this.min=min;
+    	this.max=max;
 
     }
+ 
+    lower(){
+    	this.max=this.result;
+    }
 
+    greater(){
+    	this.min=this.result;
+    }
     guess() {
-
+    	this.result=0;
+    	if((this.min-this.max)%2==0){
+          this.result=(this.min+(this.max-this.min)/2);
+         }
+        else {this.result=(this.min+(this.max-this.min+1)/2);}
+      	return this.result;
     }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
 }
 
 module.exports = GuessingGame;
